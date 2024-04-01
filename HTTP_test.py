15fc7fa00493836c390c08bad128d5390804b4c8
Feezik <92597597+Feezik@users.noreply.github.com>
2024-04-01 18:30:40 +0700

    HTTP_test.py

--- HTTP_test.py
+++ HTTP_test.py
@@ -2,10 +2,21 @@ import requests
 import json
 from datetime import datetime
 
+
+# Параметры прокси (нужно проверить работает ли прокси сервер)
+proxy_host = "189.240.60.168"
+proxy_port = 9090
+
 # URL для получения твитов пользователя
 url = 'https://api.twitter.com/graphql/GA3HM3gm-TtZJNVsvnF5Yg/UserTweets'
 
-# Параметры запроса
+
+# Настройка прокси
+proxies = {
+    "http": f"http://{proxy_host}:{proxy_port}",
+}
+
+# Параметры запроса для удаления не нужных данных
 query_params = {
     'variables': '{"userId":"44196397","count":10,"includePromotedContent":false,"withQuickPromoteEligibilityTweetFields":false ,"withVoice":true,"withV2Timeline":true}',
     'features': '{"responsive_web_graphql_exclude_directive_enabled":false,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":false,"responsive_web_graphql_timeline_navigation_enabled":false,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"communities_web_enable_tweet_community_results_fetch":false,"c9s_tweet_anatomy_moderator_badge_enabled":false,"tweetypie_unmention_optimization_enabled":false,"responsive_web_edit_tweet_api_enabled":false,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":false,"view_counts_everywhere_api_enabled":false,"longform_notetweets_consumption_enabled":false,"responsive_web_twitter_article_tweet_consumption_enabled":false,"tweet_awards_web_tipping_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":false,"standardized_nudges_misinfo":false,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":false,"rweb_video_timestamps_enabled":false,"longform_notetweets_rich_text_read_enabled":false,"longform_notetweets_inline_media_enabled":false,"responsive_web_enhance_cards_enabled":false}',
@@ -20,7 +31,7 @@ headers = {
 }
 
 # Отправить GET-запрос с токеном авторизации
-response = requests.get(url, params=query_params, headers=headers)
+response = requests.get(url, proxies=proxies, params=query_params, headers=headers)
 
 if response.status_code == 200:
     # Разобрать JSON-ответ
