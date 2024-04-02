494e200a1c5f65ae8aa7a7f0af72b7bc5070c394
Feezik <92597597+Feezik@users.noreply.github.com>
2024-04-02 18:40:16 +0700

    Update Selenium_test.py

--- Selenium_test.py
+++ Selenium_test.py
@@ -10,13 +10,13 @@ driver.get('https://www.nseindia.com/')
 time.sleep(3)
 
 # Навестись (hover) на MARKET DATA
-market_data_menu = driver.find_element('xpath', '//*[@id="link_2"]')
+market_data_menu = driver.find_element('css selector', '#link_2')
 actions = ActionChains(driver)
 actions.move_to_element(market_data_menu).perform()
 time.sleep(3)
 
 # Кликнуть на Pre-Open Market 
-pre_open_market = driver.find_element('xpath', '//*[@id="main_navbar"]/ul/li[3]/div/div[1]/div/div[1]/ul/li[1]/a')
+pre_open_market = driver.find_element('css selector', 'a[href="/market-data/pre-open-market-cm-and-emerge-market"]')
 pre_open_market.click()
 time.sleep(3)
 
@@ -26,9 +26,10 @@ with open('final_prices.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["Name", "Price"])
 
+    
 for i in range(1, 51):
-    name_element = driver.find_element('xpath', f'//*[@id="livePreTable"]/tbody/tr[{i}]/td[2]')
-    price_element = driver.find_element('xpath', f'//*[@id="livePreTable"]/tbody/tr[{i}]/td[7]')
+    name_element = driver.find_element('css selector', f'#livePreTable > tbody > tr:nth-child({i}) > td:nth-child(2) > a')
+    price_element = driver.find_element('css selector', f'#livePreTable > tbody > tr:nth-child({i}) > td.bold.text-right')
     print("Парсинг строки:", i)
 
     with open('final_prices.csv', 'a', newline='') as csvfile:
@@ -37,19 +38,21 @@ for i in range(1, 51):
 
 
 # Cымитировать небольшой пользовательский сценарий использования сайта      
-driver.find_element('xpath', '//*[@id="link_0"]').click()
+driver.find_element('css selector', '#link_0').click()
 time.sleep(2)
-driver.find_element('xpath', '//*[@id="tabList_NIFTYBANK"]').click()
+driver.find_element('css selector', '#tabList_NIFTYBANK').click()
 time.sleep(2)
 driver.execute_script("window.scrollBy(0, 500);")
 time.sleep(5)
-driver.find_element('xpath', '//*[@id="quickLinkBand"]/ul/li[2]/a').click()
+driver.find_element('css selector', '#for-investors').click()
 time.sleep(2)
-driver.find_element('xpath', '//*[@id="quickLinkBand"]/ul/li[3]/a').click()
+driver.find_element('css selector', '#for-corporate').click()
 time.sleep(4)
-driver.find_element('xpath', '//*[@id="quickLinkBand"]/ul/li[3]/a').click()
+driver.find_element('css selector', '#for-corporate').click()
 time.sleep(1)
-driver.find_element('xpath', '//*[@id="quickLinkBand"]/ul/li[1]/a').click()
+driver.execute_script("window.scrollBy(0, -500);")
+time.sleep(2)
+driver.find_element('css selector', '#tabList_NIFTYNEXT50').click()
 
 # Закрываем браузер
 time.sleep(5)
